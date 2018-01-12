7be704f1f30e852bbef54213d39dd5ab8dad5c60
BuildTools <unconfigured@null.spigotmc.org>
2018-01-12 15:00:53 +0100

    Ajouté des TODO concernant la gestion de la recuperation des datas

--- src/Data.java
+++ src/Data.java
@@ -1,36 +1,49 @@
-import java.util.ArrayList;
-import java.util.HashMap;
+
+import java.util.*;
 
 public class Data {
-    // Map of links between servers (server to which server)
-    HashMap<Integer, ArrayList<Integer>> links;
-    // List of servers per state (infected or not)
-    HashMap<Boolean, ArrayList<Integer>> infected_servers;
+    //TODO determiner une classe pour les structures de données
+    List<Object> maps;
 
-    public Data(Integer n) {
+    // Map of links between servers (server to which server)
+    Map<Integer, List<Integer>> links;
+    // List of servers per state (infected or not)
+    Map<Boolean, List<Integer>> infected_servers;
+
+    public Data() {
         // n = Number of servers
 
 
     }
 
-    void generate_data(Integer n) {
-        // generate the n servers
+    void generate_data() {
+        Random rand = new Random();
+        // Use already planned maps.
+        // 3 maps de base
+        int map = rand.nextInt(3);
+
+        // TODO plannifier des maps a tirer aleatoires
+
+        // generate the n uninfected servers
         this.infected_servers = new HashMap<>();
-        
+
+        // Server list generation
+        List<Integer> server_list = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+            server_list.add(i);
+
+        // Servers links generation
+
+        Map<Integer, List<Integer>> links = new HashMap<>();
+
+
+        this.infected_servers.put(true, new ArrayList<>());
+        this.infected_servers.put(false, new ArrayList<>());
+
     }
 
-    public void print_server(){
-        System.out.println(
-                "Les serveurs infectés :\n" +infected_servers.get(true)+ "\n" +
-                "Les serveurs non-infectés :\n" +infected_servers.get(false)+ "\n"
-        );
+    // TODO recuperer des maps
 
-        for(int i = 0; i < this.links.size(); i++){
-            System.out.println(
-                    "\nLe serveur n°" +i+ " est relié aux serveurs " +links.get(i)
-            );
-        }
-    }
-
+    // TODO
 
 }
