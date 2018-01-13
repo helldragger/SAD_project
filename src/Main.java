01ace295ad13f8d88243238465cc8fdcc564a2cf
helldragger <christopher1223334444@live.fr>
2018-01-13 12:14:56 +0100

    Loaded maps and added a map preview in the main loop to let us Conquer further bugs :D

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args){
+    	// Load maps into memory
+    	Data.load_maps();
+    	
+    	// generate a random map to play
+        Data map = Data.generate_map();
         
+        // show the game
+	    map.print_server();
     }
 }
