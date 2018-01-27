0f0b5c16725a05cf1dee4eabd25e3e8d28c96037
helldragger <christopher1223334444@live.fr>
2018-01-27 02:14:39 +0100

    Implemented a gamespeed setting.

---
+++ src/SAD/Game/GameSpeed.java
@@ -0,0 +1,16 @@
+package SAD.Game;
+
+public enum GameSpeed {
+	INSTANT(0),
+	FASTER(150),
+	FAST(300),
+	NORMAL(500),
+	SLOW(750),
+	SLOWER(1000);
+	
+	int step_duration;
+	
+	GameSpeed(int value) {
+		this.step_duration = value;
+	}
+}
